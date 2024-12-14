e6a2ca6f028f496fbe10d8ca41dcae475434af70
Anirudh Vempati <rudy.vempati@gmail.com>
2024-12-14 19:04:07 +0530

    Added (hacky) support for running GCG on llava-v1.5

--- nanogcg/gcg.py
+++ nanogcg/gcg.py
@@ -469,6 +469,320 @@ class GCG:
 
         return torch.cat(all_loss, dim=0)
 
+class GCGVLM(GCG):
+    def __init__(
+        self, 
+        model: transformers.PreTrainedModel,
+        processor: transformers.AutoProcessor,
+        config: GCGConfig,
+    ):
+        self.model = model
+        self.processor = processor
+        self.config = config
+
+        self.embedding_layer = model.get_input_embeddings()
+        self.not_allowed_ids = None if config.allow_non_ascii else get_nonascii_toks(processor, device=model.device)
+        self.prefix_cache = None
+
+        self.stop_flag = False
+
+        if model.dtype in (torch.float32, torch.float64):
+            logger.warning(f"Model is in {model.dtype}. Use a lower precision data type, if possible, for much faster optimization.")
+
+        if model.device == torch.device("cpu"):
+            logger.warning("Model is on the CPU. Use a hardware accelerator for faster optimization.")
+
+        if not processor.chat_template:
+            logger.warning("Tokenizer does not have a chat template. Assuming base model and setting chat template to empty.")
+            processor.chat_template = "{% for message in messages %}{{ message['content'] }}{% endfor %}"
+    
+    def run(
+        self,
+        messages: Union[str, List[dict]],
+        images: List,
+        target: str,
+        bos_token: str = None,
+    ) -> GCGResult:
+        model = self.model
+        processor = self.processor
+        config = self.config
+
+        if config.seed is not None:
+            set_seed(config.seed)
+            torch.use_deterministic_algorithms(True, warn_only=True)
+    
+        if isinstance(messages, str):
+            messages = [{"role": "user", "content": messages}]
+        else:
+            messages = copy.deepcopy(messages)
+    
+        # Append the GCG string at the end of the prompt if location not specified
+        if not any([("{optim_str}" in t.get("text", "") for t in d["content"]) for d in messages]):
+            messages[-1]["content"].append({"type": "text", "text":"{optim_str}"})
+
+        prompt = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        inputs = processor(images=images, text=prompt, return_tensors="pt").to(model.device, torch.float16)
+
+        template = processor.decode(inputs["input_ids"][0])
+
+        # Remove the BOS token -- this will get added when tokenizing, if necessary
+        if bos_token is not None and template.startswith(bos_token):
+            template = template.replace(bos_token, "")
+
+        before_str, after_str = template.split("{optim_str}")
+
+        target = " " + target if config.add_space_before_target else target
+
+        # Tokenize everything that doesn't get optimized
+        before_ids = processor(text=before_str, padding=False, return_tensors="pt")["input_ids"].to(model.device, torch.int64)
+        after_ids = processor(text=after_str, add_special_tokens=False, return_tensors="pt")["input_ids"].to(model.device, torch.int64)
+        target_ids = processor(text=target, add_special_tokens=False, return_tensors="pt")["input_ids"].to(model.device, torch.int64)
+
+        # Embed everything that doesn't get optimized
+        embedding_layer = self.embedding_layer
+        before_embeds, after_embeds, target_embeds = [embedding_layer(ids) for ids in (before_ids, after_ids, target_ids)]
+
+        # Compute the KV Cache for tokens that appear before the optimized tokens
+        if config.use_prefix_cache:
+            with torch.no_grad():
+                output = model(input_ids=before_ids, pixel_values=inputs['pixel_values'], use_cache=True)
+                self.prefix_cache = output.past_key_values
+        
+        self.target_ids = target_ids
+        self.before_embeds = before_embeds
+        self.after_embeds = after_embeds
+        self.target_embeds = target_embeds
+        self.pixel_values = inputs['pixel_values']
+
+        # Initialize the attack buffer
+        buffer = self.init_buffer()
+        optim_ids = buffer.get_best_ids()
+
+        losses = []
+        optim_strings = []
+        
+        for _ in tqdm(range(config.num_steps)):
+            # Compute the token gradient
+            optim_ids_onehot_grad = self.compute_token_gradient(optim_ids) 
+
+            with torch.no_grad():
+
+                # Sample candidate token sequences based on the token gradient
+                sampled_ids = sample_ids_from_grad(
+                    optim_ids.squeeze(0),
+                    optim_ids_onehot_grad.squeeze(0),
+                    config.search_width,
+                    config.topk,
+                    config.n_replace,
+                    not_allowed_ids=self.not_allowed_ids,
+                )
+
+                if config.filter_ids:
+                    sampled_ids = filter_ids(sampled_ids, processor)
+
+                new_search_width = sampled_ids.shape[0]
+
+                # Compute loss on all candidate sequences 
+                batch_size = new_search_width if config.batch_size is None else config.batch_size
+                if self.prefix_cache:
+                    input_embeds = torch.cat([
+                        embedding_layer(sampled_ids),
+                        after_embeds.repeat(new_search_width, 1, 1),
+                        target_embeds.repeat(new_search_width, 1, 1),
+                    ], dim=1)
+                else:
+                    input_embeds = torch.cat([
+                        before_embeds.repeat(new_search_width, 1, 1),
+                        embedding_layer(sampled_ids),
+                        after_embeds.repeat(new_search_width, 1, 1),
+                        target_embeds.repeat(new_search_width, 1, 1),
+                    ], dim=1)
+                loss = find_executable_batch_size(self.compute_candidates_loss, batch_size)(input_embeds)
+
+                current_loss = loss.min().item()
+                optim_ids = sampled_ids[loss.argmin()].unsqueeze(0)
+
+                # Update the buffer based on the loss
+                losses.append(current_loss)
+                if buffer.size == 0 or current_loss < buffer.get_highest_loss():
+                    buffer.add(current_loss, optim_ids)
+
+            optim_ids = buffer.get_best_ids()
+            optim_str = processor.batch_decode(optim_ids)[0]
+            optim_strings.append(optim_str)
+
+            buffer.log_buffer(processor)                
+
+            if self.stop_flag:
+                logger.info("Early stopping due to finding a perfect match.") 
+                break
+              
+        min_loss_index = losses.index(min(losses)) 
+
+        result = GCGResult(
+            best_loss=losses[min_loss_index],
+            best_string=optim_strings[min_loss_index],
+            losses=losses,
+            strings=optim_strings,
+        )
+
+        return result
+
+    def init_buffer(self) -> AttackBuffer:
+        model = self.model
+        processor = self.processor
+        config = self.config
+
+        logger.info(f"Initializing attack buffer of size {config.buffer_size}...")
+
+        # Create the attack buffer and initialize the buffer ids
+        buffer = AttackBuffer(config.buffer_size)
+
+        if isinstance(config.optim_str_init, str):
+            init_optim_ids = processor(text=config.optim_str_init, add_special_tokens=False, return_tensors="pt")["input_ids"].to(model.device)
+            if config.buffer_size > 1:
+                init_buffer_ids = processor(text=INIT_CHARS, add_special_tokens=False, return_tensors="pt")["input_ids"].squeeze().to(model.device)
+                init_indices = torch.randint(0, init_buffer_ids.shape[0], (config.buffer_size - 1, init_optim_ids.shape[1]))
+                init_buffer_ids = torch.cat([init_optim_ids, init_buffer_ids[init_indices]], dim=0)
+            else:
+                init_buffer_ids = init_optim_ids
+                
+        else: # assume list
+            if (len(config.optim_str_init) != config.buffer_size):
+                logger.warning(f"Using {len(config.optim_str_init)} initializations but buffer size is set to {config.buffer_size}")
+            try:
+                init_buffer_ids = processor(text=config.optim_str_init, add_special_tokens=False, return_tensors="pt")["input_ids"].to(model.device)
+            except ValueError:
+                logger.error("Unable to create buffer. Ensure that all initializations tokenize to the same length.")
+
+        true_buffer_size = max(1, config.buffer_size) 
+
+        # Compute the loss on the initial buffer entries
+        if self.prefix_cache:
+            init_buffer_embeds = torch.cat([
+                self.embedding_layer(init_buffer_ids),
+                self.after_embeds.repeat(true_buffer_size, 1, 1),
+                self.target_embeds.repeat(true_buffer_size, 1, 1),
+            ], dim=1)
+        else:
+            init_buffer_embeds = torch.cat([
+                self.before_embeds.repeat(true_buffer_size, 1, 1),
+                self.embedding_layer(init_buffer_ids),
+                self.after_embeds.repeat(true_buffer_size, 1, 1),
+                self.target_embeds.repeat(true_buffer_size, 1, 1),
+            ], dim=1)
+
+        init_buffer_losses = find_executable_batch_size(self.compute_candidates_loss, true_buffer_size)(init_buffer_embeds)
+
+        # Populate the buffer
+        for i in range(true_buffer_size):
+            buffer.add(init_buffer_losses[i], init_buffer_ids[[i]])
+        
+        buffer.log_buffer(processor)
+
+        logger.info("Initialized attack buffer.")
+        
+        return buffer
+    
+    def compute_token_gradient(
+        self,
+        optim_ids: Tensor,
+    ) -> Tensor:
+        """Computes the gradient of the GCG loss w.r.t the one-hot token matrix.
+
+        Args:
+            optim_ids : Tensor, shape = (1, n_optim_ids)
+                the sequence of token ids that are being optimized 
+        """
+        model = self.model
+        embedding_layer = self.embedding_layer
+
+        # Create the one-hot encoding matrix of our optimized token ids
+        optim_ids_onehot = torch.nn.functional.one_hot(optim_ids, num_classes=embedding_layer.num_embeddings)
+        optim_ids_onehot = optim_ids_onehot.to(model.device, model.dtype)
+        optim_ids_onehot.requires_grad_()
+
+        # (1, num_optim_tokens, vocab_size) @ (vocab_size, embed_dim) -> (1, num_optim_tokens, embed_dim)
+        optim_embeds = optim_ids_onehot @ embedding_layer.weight
+
+        if self.prefix_cache:
+            input_embeds = torch.cat([optim_embeds, self.after_embeds, self.target_embeds], dim=1)
+            output = model(inputs_embeds=input_embeds, past_key_values=self.prefix_cache)
+        else:
+            input_embeds = torch.cat([self.before_embeds, optim_embeds, self.after_embeds, self.target_embeds], dim=1)
+            output = model(inputs_embeds=input_embeds, pixel_values=self.pixel_values)
+
+        logits = output.logits
+
+        # Shift logits so token n-1 predicts token n
+        shift = input_embeds.shape[1] - self.target_ids.shape[1]
+        shift_logits = logits[..., shift-1:-1, :].contiguous() # (1, num_target_ids, vocab_size)
+        shift_labels = self.target_ids
+
+        if self.config.use_mellowmax:
+            label_logits = torch.gather(shift_logits, -1, shift_labels.unsqueeze(-1)).squeeze(-1)
+            loss = mellowmax(-label_logits, alpha=self.config.mellowmax_alpha, dim=-1)
+        else:
+            loss = torch.nn.functional.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        optim_ids_onehot_grad = torch.autograd.grad(outputs=[loss], inputs=[optim_ids_onehot])[0]
+
+        return optim_ids_onehot_grad
+    
+    def compute_candidates_loss(
+        self,
+        search_batch_size: int, 
+        input_embeds: Tensor, 
+    ) -> Tensor:
+        """Computes the GCG loss on all candidate token id sequences.
+
+        Args:
+            search_batch_size : int
+                the number of candidate sequences to evaluate in a given batch
+            input_embeds : Tensor, shape = (search_width, seq_len, embd_dim)
+                the embeddings of the `search_width` candidate sequences to evaluate
+        """
+        all_loss = []
+        prefix_cache_batch = []
+
+        for i in range(0, input_embeds.shape[0], search_batch_size):
+            with torch.no_grad():
+                input_embeds_batch = input_embeds[i:i+search_batch_size]
+                current_batch_size = input_embeds_batch.shape[0]
+
+                if self.prefix_cache:
+                    if not prefix_cache_batch or current_batch_size != search_batch_size:
+                        prefix_cache_batch = [[x.expand(current_batch_size, -1, -1, -1) for x in self.prefix_cache[i]] for i in range(len(self.prefix_cache))]
+
+                    outputs = self.model(inputs_embeds=input_embeds_batch, past_key_values=prefix_cache_batch)
+                else:
+                    outputs = self.model(inputs_embeds=input_embeds_batch, pixel_values=self.pixel_values.repeat(current_batch_size, 1, 1, 1))
+
+                logits = outputs.logits
+
+                tmp = input_embeds.shape[1] - self.target_ids.shape[1]
+                shift_logits = logits[..., tmp-1:-1, :].contiguous()
+                shift_labels = self.target_ids.repeat(current_batch_size, 1)
+                
+                if self.config.use_mellowmax:
+                    label_logits = torch.gather(shift_logits, -1, shift_labels.unsqueeze(-1)).squeeze(-1)
+                    loss = mellowmax(-label_logits, alpha=self.config.mellowmax_alpha, dim=-1)
+                else:
+                    loss = torch.nn.functional.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1), reduction="none")
+
+                loss = loss.view(current_batch_size, -1).mean(dim=-1)
+                all_loss.append(loss)
+
+                if self.config.early_stop:
+                    if torch.any(torch.all(torch.argmax(shift_logits, dim=-1) == shift_labels, dim=-1)).item():
+                        self.stop_flag = True
+
+                del outputs
+                gc.collect()
+                torch.cuda.empty_cache()
+
+        return torch.cat(all_loss, dim=0)
+
 # A wrapper around the GCG `run` method that provides a simple API
 def run(
     model: transformers.PreTrainedModel,
@@ -497,4 +811,35 @@ def run(
     gcg = GCG(model, tokenizer, config)
     result = gcg.run(messages, target)
     return result
+
+# A wrapper around the GCGVLM `run` method that provides a simple API
+
+def runVLM(
+    model: transformers.PreTrainedModel,
+    processor: transformers.AutoProcessor,
+    messages: Union[str, List[dict]],
+    target: str,
+    images: List = None,
+    config: Optional[GCGConfig] = None,
+    bos_token: str = None,
+) -> GCGResult:
+    """Generates a single optimized string using GCG. 
+
+    Args:
+        model: The model to use for optimization.
+        processor: The model's processor.
+        messages: The conversation to use for optimization.
+        target: The target generation.
+        config: The GCG configuration to use.
     
+    Returns:
+        A GCGResult object that contains losses and the optimized strings.
+    """
+    if config is None:
+        config = GCGConfig()
+    
+    logger.setLevel(getattr(logging, config.verbosity))
+    
+    gcg = GCGVLM(model, processor, config)
+    result = gcg.run(messages, images, target, bos_token)
+    return result
